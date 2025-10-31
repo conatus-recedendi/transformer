6b0bd5d3eaef414925b35513094da48a22fdf606
kyle <kyle@wrtn.io>
2025-10-31 18:10:02 +0900

    fix

--- src/positional_encoding.py
+++ src/positional_encoding.py
@@ -30,10 +30,10 @@ class PositionalEncoding(nn.Module):
         # "step=2" means 'i' multiplied with two (same with 2 * i)
 
         self.encoding[:, 0::2] = torch.sin(
-            pos / (10000 ** (_2i / d_model)), device=device
+            pos / (10000 ** (_2i / d_model)),
         )
         self.encoding[:, 1::2] = torch.cos(
-            pos / (10000 ** (_2i / d_model)), device=device
+            pos / (10000 ** (_2i / d_model)),
         )
         # compute positional encoding to consider positional information of words
 
